2ba72aa098c04e877b8ecc8919ad03ed64fb6619
Clink 123 <jameslepone@gmail.com>
2021-08-11 11:50:42 -1000

    Add email form and mobile hero

--- index.php
+++ index.php
@@ -1,1 +1,335 @@
-<?php include_once("home.html"); ?>
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="description" content="BigRed//Hacks - Be a part of something big.">
+  <meta name="author" content="BigRed//Hacks">
+  <link rel="shortcut icon" href="assets/favicon.ico">
+  <meta property="og:site_name" content="BigRed//Hacks">
+  <meta property="og:description"
+    content="BigRed//Hacks - Be a part of something big. Happening at Cornell University on September 24th-26th, 2021.">
+  <meta property="og:url" content="https://www.bigredhacks.com">
+  <meta property="og:title" content="BigRed//Hacks">
+  <meta property="og:image" content="https://www.bigredhacks.com/assets/main.png">
+  <meta property="og:image:type" content="image/png">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@bigredhacks">
+  <meta name="twitter:creator" content="@bigredhacks">
+  <meta name="twitter:title" content="BigRed//Hacks">
+  <meta name="twitter:description"
+    content="BigRed//Hacks - Be a part of something big. Happening at Cornell University on September 24th-26th, 2021.">
+  <meta name="twitter:image" content="https://www.bigredhacks.com/assets/main.png">
+  <title>BigRed//Hacks </title>
+  <link rel="stylesheet" href="styles/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link href="/styles/normalize.min.css">
+  <link href="styles/main.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://s.pageclip.co/v1/pageclip.css" media="screen">
+</head>
+<a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;right:20px;top:0;width:10%;z-index:10000" href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg" alt="Major League Hacking 2022 Hackathon Season" style="width:100%"></a>
+
+<body>
+  <div id="navbar">
+    <a href="#hero" class="navbar-item hero">Home</a>
+    <a href="#about" class="navbar-item about">About</a>
+    <a href="#schedule" class="navbar-item schedule">Schedule</a>
+    <a href="#faq" class="navbar-item faq">FAQ</a>
+    <a href="#sponsors" class="navbar-item sponsors">Sponsors</a>
+    <a href=" https://apply.bigredhacks.com/" target=_blank class="disabled navbar-item is-bold apply">Apply</a>
+  </div>
+
+  <section id="hero"></section>
+
+  <section id="about">
+    <div class="container">
+      <h1 class="section__title">Who Are We</h1>
+      <div class="about__content">
+        <div class="inner">
+          <p>BigRed//Hacks is the oldest student-run, large-scale hackathon at Cornell
+            University.</p>
+          <p>Across three days, Cornell and non-Cornell students alike gather to build the
+            coolest and most innovative
+            projects, meet amazing people, and win some prizes along the way!</p>
+		  <br>
+          <p>Applications Open Soon! <a target="_blank" href="https://forms.gle/SkpiSVALfxUyKnVf6" class="is-bold link">Get Notified</a></p>
+          <a href="https://apply.bigredhacks.com/" class="disabled is-bold">Interested in attending?
+            Apply here!</a>
+        </div>
+        <div class="inner">
+          <img alt="Bear and trees graphic" src="assets/bear+trees.png">
+        </div>
+      </div>
+      <div class="previous__spotlight">
+        <div class="previous__image">
+          <img src="assets/org-2019.jpg" alt="2019 BRH participants photo">
+        </div>
+        <div class="previous__image">
+          <img src="assets/org-2018temp.jpg" alt="2018 BRH cupstacking team photo">
+        </div>
+        <div class="previous__image">
+          <img src="assets/org-2017temp.jpg" alt="2017 BRH IBM workshop photo">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="schedule">
+    <div class="container">
+      <h1 class="section__title">Event Schedule</h1>
+
+      <div class="schedule__container">
+        <div class="schedule__day">
+          <p class="day">Friday, September 24th</p>
+          <table>
+            <tbody>
+              <tr>
+                <th>Time</th>
+                <th>Event</th>
+              </tr>
+              <tr>
+                <td>4:00 PM</td>
+                <td>Check-In Begins</td>
+              </tr>
+              <tr>
+                <td>6:00 PM</td>
+                <td>Opening Ceremony</td>
+              </tr>
+              <tr>
+                <td>7:00 PM</td>
+                <td>Dinner</td>
+              </tr>
+              <tr>
+                <td>8:00 PM</td>
+                <td>Hacking Begins/Team Formation</td>
+              </tr>
+              <tr>
+                <td>10:00 PM</td>
+                <td>Online Social</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="schedule__day">
+          <p class="day">Saturday, September 25th</p>
+          <table>
+            <tbody>
+              <tr>
+                <th>Time</th>
+                <th>Event</th>
+              </tr>
+              <tr>
+                <td>12:00 AM</td>
+                <td>Midnight Snack</td>
+              </tr>
+              <tr>
+                <td>8:00 AM</td>
+                <td>Midnight Snack</td>
+              </tr>
+              <tr>
+                <td>9:00 AM</td>
+                <td>Bebe Lake Nature Walk</td>
+              </tr>
+              <tr>
+                <td>12:00 PM</td>
+                <td>Lunch</td>
+              </tr>
+              <tr>
+                <td>3:00 PM</td>
+                <td>T-Shirts!</td>
+              </tr>
+              <tr>
+                <td>5:00 PM</td>
+                <td>Cup Stacking</td>
+              </tr>
+              <tr>
+                <td>7:00 PM</td>
+                <td>Dinner</td>
+              </tr>
+              <tr>
+                <td>9:00 PM</td>
+                <td>Suprise Event</td>
+              </tr>
+              <tr>
+                <td>11:00 PM</td>
+                <td>Online Games</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="schedule__day">
+          <p class="day">Sunday, September 26th</p>
+          <table>
+            <tbody>
+              <tr>
+                <th>Time</th>
+                <th>Event</th>
+              </tr>
+              <tr>
+                <td>1:00 AM</td>
+                <td>Midnight Snack</td>
+              </tr>
+              <tr>
+                <td>8:00 AM</td>
+                <td>Hacking Ends/Breakfast</td>
+              </tr>
+              <tr>
+                <td>9:00 AM</td>
+                <td>Demos!</td>
+              </tr>
+              <tr>
+                <td>11:00 AM</td>
+                <td>Lunch</td>
+              </tr>
+              <tr>
+                <td>12:00 PM</td>
+                <td>Closing Ceremony</td>
+              </tr>
+              <tr>
+                <td>2:00 PM</td>
+                <td>Pack Up & Goodbyes</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <i>All Events Subject To Change.</i>
+      <img alt="Trees graphic" src="assets/trees.png">
+    </div>
+  </section>
+
+  <section id="faq">
+    <div class="container">
+      <h1 class="section__title">Frequently Asked Questions</h1>
+      <div class="faq__container">
+        <div class="inner">
+          <div class="faq__single">
+            <p class="question is-bold">What are your protocols for COVID-19?</p>
+            <p class="answer">BigRed//Hacks will be an in-person event to the fullest extent that CDC, New York State,
+              and Cornell University guidelines permit. We expect to be able to accept all vaccinated Cornell students
+              in person, and are working towards accepting all vaccinated students from other colleges in the US as
+              well. We will keep you up to date as soon as we know who will be able to attend in person. Fear not, as
+              the event will be fully hybrid, and allow for a full experience virtually as well. So even if you can't or
+              feel uncomfortable attending in-person, there will be a full online component to the event for you to
+              participate in.</p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">Who is eligible to participate?</p>
+            <p class="answer">We are inviting all undergraduate and graduate students to
+              participate in our hackathon.
+              You don't need any past experience. You don't even need to be a computer science major! Our only
+              requirement is that you are at least 18 years old and enrolled in an accredited college or university.
+            </p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">What will I need to bring? </p>
+            <p class="answer">If you are participating online, all you'll need is a stable internet connection and your
+              computer! If in person, make sure you have been accepted as an in-person attendee (details will be
+              released on this during the application period), and bring your school ID for registration. You are also
+              responsible for bringing any equipment for building and demoing your hack: laptop, chargers, phones, etc.
+              You may also want to bring a sleeping bag, change of clothes, and toothbrush.
+            </p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">Do I need to sign up with a team?</p>
+            <p class="answer">You don't have to sign up with a team, but it's certainly more
+              fun! We allow teams of up
+              to four people (they don't have to be from the same school), and while they can be specified on the
+              application, you won't officially, create your team until the event. If you don't have a team yet, we'll
+              host a social before the hacking begins for you to meet other amazing people.
+            </p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">What are the rules?</p>
+            <p class="answer">Detailed hack specific rules will be coming out closer to the event, but generally all
+              hacks must be safe, must follow the MLH code of conduct, and should follow the theme of the event, where
+              applicable. More details coming soon.
+            </p>
+          </div>
+        </div>
+
+        <div class="inner">
+
+          <div class="faq__single">
+            <p class="question is-bold">Why is there an application?</p>
+            <p class="answer">We want to ensure that all hackers will have a positive experience at BigRed//Hacks.
+              Further, we need to know your T-shirt size and dietary information for swag and meal options! Don't worry,
+              the application is short.</p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">Are hardware hacks allowed?</p>
+            <p class="answer">We will be providing a dedicated hardware lab with
+              sensors, microcontrollers, and basic components. Some of our sponsors are bringing hardware to loan, but
+              we still suggest that you bring your own equipment if you know that you'll absolutely need it.</p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">How does travel reimbursement work?</p>
+            <p class="answer">Unfortunately, we unable to provide travel reimbursements or busses for 2021. The
+              in-person component of BigRed//Hacks is expected to be more heavily weighted towards Cornell students due
+              to COVID-19 restrictions. Where travel is a burden or impossibility, our event will feature a fully online
+              component with similar experiences to those found in-person. So fear not, you can have a similar hackathon
+              experience virtually!</p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">Are you providing food?</p>
+            <p class="answer"> Don't worry, there will be lots of delicious food and beverages
+              at our event, complimentary to all participants. We'll have vegetarian options as well. </p>
+          </div>
+          <div class="faq__single">
+            <p class="question is-bold">Have more questions?</p>
+            <p class="answer">Send them to info@bigredhacks.com!</p>
+          </div>
+          <img alt="Planets graphic" src="assets/planets.png">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="sponsors">
+    <div class="container">
+      <h1 class="section__title">Sponsors</h1>
+      <div class="sponsors__area">
+        <a href="https://mlh.io/" target=_blank>
+          <img alt="MLH Logo" class="sponsor" src="assets/sponsors/mlh-logo-color.png">
+        </a>
+        <a href="https://www.theforage.com/forage-for-funds/cornell-big-red-hacks" target=_blank>
+          <img alt="Forage Logo" class="sponsor" src="assets/sponsors/forage.png">
+        </a>
+        <a href="https://www.linode.com/" target=_blank>
+          <img alt="Linode Logo" class="sponsor" src="assets/sponsors/linode.png">
+        </a>
+        <a href="https://www.ubs.com/us/en.html" target=_blank>
+          <img alt="UBS Logo" class="sponsor" src="assets/sponsors/ubs.jpg">
+        </a>
+      </div>
+      <div class="sponsors__callout">
+        <h2 class="is-bold">Want to sponsor us?</h2>
+        <p>We're still accepting sponsors for our 2021 event!</p>
+        <p> Send us an email at <a class="is-bold" href="mailto: info@bigredhacks.com">info@bigredhacks.com</a></p>
+      </div>
+    </div>
+  </section>
+
+  <div id="footer">
+    <div class="footer-img"><img alt="BRH logo" src="assets/logo.png"></div>
+    <div class="btn-social-group">
+      <a class="btn btn-social" href="https://facebook.com/bigredhacks" target="_blank"><i
+          class="fa fa-facebook"></i></a>
+      <a class="btn btn-social" href="https://twitter.com/bigredhacks" target="_blank"><i class="fa fa-twitter"
+          aria-hidden="true"></i></a>
+      <a class="btn btn-social" href="mailto: info@bigredhacks.com" target="_blank"><i class="fa fa-envelope"
+          aria-hidden="true"></i></a>
+      <p>Made with 🤍 from the <a href="https://org.bigredhacks.com/" target=_blank>BigRed//Hacks team</a></p>
+      <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target=_blank>MLH Code of Conduct</a>
+    </div>
+  </div>
+  <script src="scripts/index.js"></script>
+  <script src="https://s.pageclip.co/v1/pageclip.js" charset="utf-8"></script>
+</body>
+
+</html>
